20346571b2c8b39dca99a76e2412bbbddad4e2ba
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-02-19 14:25:45 +0500

    Update 20260219_01_add_owner_id_to_companies.py

--- review_saas/igrations/versions/20260219_01_add_owner_id_to_companies.py
+++ review_saas/igrations/versions/20260219_01_add_owner_id_to_companies.py
@@ -5,14 +5,19 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '20260219_01_add_owner_id_to_companies'
-down_revision = '20260218_03_last_migration'  # <- set this to your last applied migration
+
+# <-- Set this to your last applied migration’s revision ID
+down_revision = '20260218_03_create_companies_table'  # replace with your actual last migration ID
 branch_labels = None
 depends_on = None
 
 def upgrade():
-    # Add the owner_id column to companies table
-    op.add_column('companies', sa.Column('owner_id', sa.Integer(), nullable=True))
+    """Add owner_id column to companies table"""
+    op.add_column(
+        'companies',
+        sa.Column('owner_id', sa.Integer(), nullable=True)
+    )
 
 def downgrade():
-    # Remove the owner_id column if you roll back
+    """Remove owner_id column from companies table"""
     op.drop_column('companies', 'owner_id')
