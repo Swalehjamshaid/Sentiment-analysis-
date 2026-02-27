821571cf252d51eef777d305175c876f9f9d0356
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-02-27 22:02:08 +0500

    Delete review_saas/app directory

--- review_saas/app/routes/admin.py
+++
@@ -1,8 +0,0 @@
-# app/routes/admin.py
-
-from fastapi import APIRouter
-router = APIRouter(prefix="/admin", tags=["admin"])
-
-@router.get("/")
-def admin_panel():
-    return {"message": "Admin panel"}
