22322b44ea04bb89fbff04f06995288e73e4a953
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-02-28 14:32:43 +0500

    Delete review_saas directory

--- review_saas/app/utilities/sanitize.py
+++
@@ -1,7 +0,0 @@
-# filename: app/utilities/sanitize.py
-import bleach
-
-def sanitize_text(value: str) -> str:
-    if not value:
-        return value
-    return bleach.clean(value, strip=True)
--- review_saas/app/wsgi.py
+++
@@ -1,3 +0,0 @@
-# filename: app/wsgi.py
-from app import create_app
-app = create_app()
