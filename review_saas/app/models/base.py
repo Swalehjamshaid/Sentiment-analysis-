563ba4ee163246ae0da735ac66774d78c3707b8b
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-02-28 16:54:07 +0500

    Update base.py

--- review_saas/app/models/base.py
+++ review_saas/app/models/base.py
@@ -1,4 +1,6 @@
-
 # filename: app/models/base.py
 from sqlalchemy.orm import declarative_base
+
+# Requirement #124: Centralized Base for all SQLAlchemy models
+# This object keeps track of all tables defined in your models.py
 Base = declarative_base()
