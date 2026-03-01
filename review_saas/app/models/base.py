fd714dddcab0b6a92bd138b79f9ac29fd99bf738
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-03-02 00:20:51 +0500

    Delete review_saas directory

--- review_saas/app/models/base.py
+++
@@ -1,7 +0,0 @@
-# File: review_saas/app/models/base.py
-from sqlalchemy.orm import declarative_base
-# Fixed import path:
-from app.core.settings import settings
-
-# Requirement #124: Centralized Base for all SQLAlchemy models
-Base = declarative_base()
