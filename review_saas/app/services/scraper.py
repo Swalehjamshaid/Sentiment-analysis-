192d8dc678dee9133113c539b82ba05350bcce97
Swalehjamshaid <roy.jamshaid@gmail.com>
2026-05-14 15:07:35 +0500

    Update scraper.py

--- review_saas/app/services/scraper.py
+++ review_saas/app/services/scraper.py
@@ -293,7 +293,8 @@ async def fetch_reviews_from_google(
         run = await asyncio.to_thread(
             lambda:
             apify_client.actor(
-               alexey/google-maps-reviews-scraper
+    "alexey/google-maps-reviews-scraper"
+)
             ).call(
                 run_input=run_input
             )
